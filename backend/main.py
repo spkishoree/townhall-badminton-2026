53a4a0d06d5f1e1bbc52599507fdc832b798fc63
kishore <kishorecena444@gmail.com>
2026-08-03 11:37:27 +0530

    CORS CHANGE

--- backend/main.py
+++ backend/main.py
@@ -18,9 +18,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://townhall-badminton-2026.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
